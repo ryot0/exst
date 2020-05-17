25a87fa573bcd4adfaebc0f639afdfd595fd5c51
ryoto naruse <naruse.com@gmail.com>
2020-05-17 20:53:12 +0900

    システム関連のワードを追加

--- exst_core/src/primitive/system.rs
+++ exst_core/src/primitive/system.rs
@@ -3,9 +3,54 @@
 //! 
 
 use super::super::lang::vm::*;
+use super::super::lang::resource::*;
+use super::util;
 
 /// システム関連ワードを登録
-pub fn initialize<V>(_: &mut V)
+pub fn initialize<V>(vm: &mut V)
     where V: VmPrimitiveWordStore
 {
+    vm.define_primitive_word("exit".to_string(), false, exit);
+    vm.define_primitive_word("bye".to_string(), false, bye);
+    vm.define_primitive_word("[".to_string(), true, to_interpletation);
+    vm.define_primitive_word("]".to_string(), false, to_compilation);
+    vm.define_primitive_word("load".to_string(), false, load);
+}
+
+/// モジュールの終了
+fn exit<V: VmManipulation,E>(vm: &mut V) -> Result<(),VmErrorReason<E>>
+{
+    vm.set_state(VmState::Return);
+    Result::Ok(())
+}
+
+/// プログラムの終了
+fn bye<V: VmManipulation,E>(vm: &mut V) -> Result<(),VmErrorReason<E>>
+{
+    vm.set_state(VmState::Stop);
+    Result::Ok(())
+}
+
+/// コンパイルモードへ遷移
+fn to_compilation<V: VmManipulation,E>(vm: &mut V) -> Result<(),VmErrorReason<E>>
+{
+    vm.set_state(VmState::Compilation);
+    Result::Ok(())
+}
+
+/// コンパイルモードへ遷移
+fn to_interpletation<V: VmManipulation,E>(vm: &mut V) -> Result<(),VmErrorReason<E>>
+{
+    vm.set_state(VmState::Interpretation);
+    Result::Ok(())
+}
+
+/// モジュールのロード
+fn load<V: VmManipulation,E>(vm: &mut V) -> Result<(),VmErrorReason<E>>
+{
+    util::call_with_name(vm, |v, name|{
+        let target = v.resources().get_token_iterator(&name)?;
+        v.call_script(target);
+        Result::Ok(())
+    })
 }
--- exst_core/src/primitive/util.rs
+++ exst_core/src/primitive/util.rs
@@ -1,8 +1,14 @@
 
+//!
+//! 組み込みワードを定義するための補助関数
+//! 
+
 use super::super::lang::vm::*;
+use super::super::lang::tokenizer::*;
 use super::super::lang::value::*;
 use std::rc::Rc;
 
+/// ( int int -- int )のワードの呼び出し
 pub fn call_iifi<V,E,F>(vm: &mut V, f: F) -> Result<(),VmErrorReason<E>>
     where V: VmManipulation,
           F: Fn(i32,i32) -> i32
@@ -24,5 +30,28 @@ pub fn call_iifi<V,E,F>(vm: &mut V, f: F) -> Result<(),VmErrorReason<E>>
         _ => {
             Result::Err(VmErrorReason::TypeMismatchError(int_type_name(), lhs.type_name()))
         }
+    }
+}
+
+/// 次のsymbol tokenを使用するワードの呼び出し
+pub fn call_with_name<V,E,F>(vm: &mut V, f: F) -> Result<(),VmErrorReason<E>>
+    where V: VmManipulation,
+          F: Fn(&mut V, &String) -> Result<(),VmErrorReason<E>>
+{
+    match vm.input_stream_mut().next_token() {
+        Option::Some(t) => {
+            let token = t.value_token?;
+            match token {
+                ValueToken::Symbol(name) => {
+                    f(vm, &name)
+                },
+                _ => {
+                    Result::Err(VmErrorReason::TypeMismatchError("Symbol", "UNKNOWN"))
+                }
+            }
+        },
+        Option::None => {
+            Result::Err(VmErrorReason::TypeMismatchError("Symbol", "EMPTY"))
+        },
     }
 }
