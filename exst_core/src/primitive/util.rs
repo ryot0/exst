d76ca0acec9d3274ec59760a9607eb6f3c964033
ryoto naruse <naruse.com@gmail.com>
2020-06-04 21:56:07 +0900

    lhsとrhsの解釈が逆になっていたバグを修正

--- exst_core/src/primitive/util.rs
+++ exst_core/src/primitive/util.rs
@@ -8,14 +8,27 @@ use super::super::lang::tokenizer::*;
 use super::super::lang::value::*;
 use std::rc::Rc;
 
+/// ( x x -- Result )のワードの呼び出し
+fn call_xxfr<V,E,F>(vm: &mut V, f: F) -> Result<(),VmErrorReason<E>>
+    where V: VmManipulation,
+          F: Fn(Rc<Value<V::ExtraValueType>>,Rc<Value<V::ExtraValueType>>) -> Result<Value<V::ExtraValueType>,VmErrorReason<E>>,
+          E: std::fmt::Debug
+{
+    let rhs = vm.data_stack_mut().pop()?;
+    let lhs = vm.data_stack_mut().pop()?;
+    let v = f(lhs, rhs)?;
+    vm.data_stack_mut().push(Rc::new(v));
+    Result::Ok(())
+}
+
 /// ( x x -- x )のワードの呼び出し
 pub fn call_xxfx<V,E,F>(vm: &mut V, f: F) -> Result<(),VmErrorReason<E>>
     where V: VmManipulation,
           F: Fn(Rc<Value<V::ExtraValueType>>,Rc<Value<V::ExtraValueType>>) -> Value<V::ExtraValueType>,
           E: std::fmt::Debug
 {
-    let lhs = vm.data_stack_mut().pop()?;
     let rhs = vm.data_stack_mut().pop()?;
+    let lhs = vm.data_stack_mut().pop()?;
     vm.data_stack_mut().push(Rc::new(f(lhs, rhs)));
     Result::Ok(())
 }
@@ -37,24 +50,23 @@ pub fn call_iifi<V,E,F>(vm: &mut V, f: F) -> Result<(),VmErrorReason<E>>
           F: Fn(i32,i32) -> i32,
           E: std::fmt::Debug
 {
-    let lhs = vm.data_stack_mut().pop()?;
-    match *lhs {
-        Value::IntValue(lhs) => {
-            let rhs = vm.data_stack_mut().pop()?;
-            match *rhs {
-                Value::IntValue(rhs) => {
-                    vm.data_stack_mut().push(Rc::new(Value::IntValue(f(lhs, rhs))));
-                    Result::Ok(())
-                },
-                _ => { 
-                    Result::Err(VmErrorReason::TypeMismatchError(int_type_name(), rhs.type_name()))
-                 }
+    call_xxfr(vm, |lhs,rhs|{
+        match *rhs {
+            Value::IntValue(rhs) => {
+                match *lhs {
+                    Value::IntValue(lhs) => {
+                        Result::Ok(Value::IntValue(f(lhs, rhs)))
+                    },
+                    _ => { 
+                        Result::Err(VmErrorReason::TypeMismatchError(int_type_name(), lhs.type_name()))
+                     }
+                }
+            },
+            _ => {
+                Result::Err(VmErrorReason::TypeMismatchError(int_type_name(), rhs.type_name()))
             }
-        },
-        _ => {
-            Result::Err(VmErrorReason::TypeMismatchError(int_type_name(), lhs.type_name()))
         }
-    }
+    } )
 }
 
 /// 次のsymbol tokenを使用するワードの呼び出し
